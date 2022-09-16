92477639a1aedbe9e770edc9ded7c43f6e46c29e
OleksandrS <sai7602@ukr.net>
2022-09-16 21:30:23 +0300

    create row template and add svg

---
+++ src/js/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    catId: 1,
+    catName: 'Task',
+    catImg: 'shopping-cart-svgrepo-com',
+  },
+  {
+    catId: 2,
+    catName: 'Random Thought',
+    catImg: 'idea-think-svgrepo-com',
+  },
+  {
+    catId: 3,
+    catName: 'Idea',
+    catImg: 'idea-svgrepo-com',
+  },
+  {
+    catId: 4,
+    catName: 'Quote',
+    catImg: 'quote-right-svgrepo-com',
+  },
+];
+
+export default categories;
---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+import getDate from './getDate';
+console.log(getDate());
